fd37e8f96d748cad8b392970944771877857c81c
Joshua Chinemezu <joshuachinemezu@gmail.com>
2024-03-09 13:14:59 +0000

    Update pkgs

--- lib/src/utils/location_utils.dart
+++ lib/src/utils/location_utils.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:flutter/services.dart';
 
 class LocationUtils {
